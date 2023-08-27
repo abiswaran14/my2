0caedc6b65d436582ab62fb9b18ad0ea7e6c87ca
abiswaran14 <143321236+abiswaran14@users.noreply.github.com>
2023-08-27 22:45:03 +0530

    Add files via upload

---
+++ new.py
@@ -0,0 +1,32 @@
+class security():
+    a=input("enter your username")
+    b=input("enter your password")
+    username=input("enter your username")
+    password=input("enter your password")
+    a == username
+    b == password
+    if a==username and b==password:
+        print("username and password is correct")
+        print(l)
+    else:
+        print("username and password is wrong")
+
+class prog():
+    name="good name"
+    phone="1234556"
+    add="good add"
+    print(name)
+    print(phone)
+    print(add)
+
+i=security()
+print(i)
+l=prog()
+print(l)
+'''''
+if :
+    print(l)
+else:
+    print("************")
+    print("acess denied")
+'''
